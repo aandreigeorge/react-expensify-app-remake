5fe1b4f3384ecc55002f20f3aa9ad1c6b7567f80
Andrei George Avram <andreiavram91@gmail.com>
2023-03-21 19:25:11 +0200

    Added styles for inputs

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -24,40 +24,43 @@ class ExpenseListFilters extends React.Component {
                 <div className='input-group'>
                     <div className='input-group__item'>
                         <input 
-                        type='text' 
-                        value={this.props.filters.text}
-                        onChange = {(e) => {
-                            this.props.dispatch(setTextFilter(e.target.value));
-                        }}
+                            type='text' 
+                            className='text-input'
+                            placeholder='Search expenses'
+                            value={this.props.filters.text}
+                            onChange = {(e) => {
+                                this.props.dispatch(setTextFilter(e.target.value));
+                            }}
                         />
                     </div>
                     <div className='input-group__item'>
-                        <select 
-                        value={this.props.filters.sortBy}
-                        onChange = {(e) => {
-                            if(e.target.value === 'date') {
-                                this.props.dispatch(sortByDate());
-                            } else if (e.target.value === 'amount') {
-                                this.props.dispatch(sortByAmount());
-                            }
-                        }}
+                        <select
+                            className='select'
+                            value={this.props.filters.sortBy}
+                            onChange = {(e) => {
+                                if(e.target.value === 'date') {
+                                    this.props.dispatch(sortByDate());
+                                } else if (e.target.value === 'amount') {
+                                    this.props.dispatch(sortByAmount());
+                                }
+                            }}
                         >
-                        <option value='date'>Date</option>
-                        <option value='amount'>Anmount</option>
+                            <option value='date'>Date</option>
+                            <option value='amount'>Anmount</option>
                         </select>
                     </div>
                     <div className='input-group__item'>
                         <DateRangePicker 
-                        startDate={this.props.filters.startDate}
-                        startDateId={'start'}
-                        endDate={this.props.filters.endDate}
-                        endDateId={'end'}
-                        onDatesChange={this.onDatesChange}
-                        focusedInput={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={ 1 }
-                        isOutsideRange={ () => false }
-                        showClearDates={ true }
+                            startDate={this.props.filters.startDate}
+                            startDateId={'start'}
+                            endDate={this.props.filters.endDate}
+                            endDateId={'end'}
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            numberOfMonths={ 1 }
+                            isOutsideRange={ () => false }
+                            showClearDates={ true }
                         /> 
                     </div>
                 </div>
